a0f05a7741aa0b0c6aa65a82b5c7b3c392224142
amir <amir15bfk@gmail.com>
2021-07-23 21:09:59 +0100

    change getters to give a list of all history of data

--- api.py
+++ api.py
@@ -12,16 +12,41 @@ response = requests.request("GET", url, headers=headers).json()
 
 
 def get_new_deaths():
-    return response["stats"]["newDeaths"]
+    r =[]
+    for i,item in enumerate(response["stats"]["history"]):
+        r.append(item['deaths']-response["stats"]["history"][i-1]['deaths'])
+    r[0]=0
+    return r
 
-def get_new_Cases():
-    return response["stats"]["newlyConfirmedCases"]
+def get_new_cases():
+    r =[]
+    for i,item in enumerate(response["stats"]["history"]):
+        r.append(item['confirmed']-response["stats"]["history"][i-1]['confirmed'])
+    r[0]=0
+    return r
 
-def get_total_Cases():
-    return response["stats"]["totalConfirmedCases"]
+def get_new_recovered_cases():
+    r =[]
+    for i,item in enumerate(response["stats"]["history"]):
+        r.append(item['recovered']-response["stats"]["history"][i-1]['recovered'])
+    r[0]=0
+    return r
 
-def get_tota_lDeaths():
-    return response["stats"]["totalDeaths"]
+def get_total_recovered_cases():
+    r =[]
+    for item in response["stats"]["history"]:
+        r.append(item['recovered'])
+    return r
 
+def get_total_cases():
+    r =[]
+    for item in response["stats"]["history"]:
+        r.append(item['confirmed'])
+    return r
+    
+def get_total_deaths():
+    r =[]
+    for item in response["stats"]["history"]:
+        r.append(item['deaths'])
+    return r
 
-print(get_new_deaths())
